eb683bcf29945bb10756c7351ae0811109779485
Leon Gaban <leongaban@gmail.com>
2024-06-19 23:10:46 -0500

    init commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <RouterLink to="/">Notes</RouterLink> |
+  <RouterLink to="/stats">Stats</RouterLink>
+  <RouterView />
+</template>
+
+<style scoped></style>
